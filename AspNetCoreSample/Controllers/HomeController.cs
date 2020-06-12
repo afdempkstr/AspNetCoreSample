3031522d504f913b6594d62346da11be05ca38ab
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-12 22:02:08 +0300

    Added partial view for candidate

---
+++ AspNetCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreSample.Models;
+using AspNetCoreSample.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public HomeController(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var model = new HomePageViewModel()
+            {
+                Header = "Popular candidates",
+                PopularCandidates = _candidateRepository.PopularCandidates()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ AspNetCoreSample/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AspNetCoreSample.Models;
+using System.Collections.Generic;
+
+namespace AspNetCoreSample.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public string Header { get; set; }
+
+        public IEnumerable<Candidate> PopularCandidates { get; set; }
+    }
+}
