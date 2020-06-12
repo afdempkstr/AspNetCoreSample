f591c16d79e699841aa906ac0bcfa4f5d4552d13
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-12 19:02:19 +0300

    Added Db Context

---
+++ AspNetCoreSample/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreSample.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        public DbSet<Specialisation> Specialisations { get; set; }
+
+    }
+}
--- AspNetCoreSample/Startup.cs
+++ AspNetCoreSample/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,10 +17,20 @@ namespace AspNetCoreSample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<ICandidateRepository, MockCandidateRepository>();
             services.AddScoped<ISpecialisationRepository, MockSpecialisationRepository>();
 
