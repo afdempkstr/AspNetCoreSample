5f51788ce78762a7efca08d47aa06e94ac97e312
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-16 22:06:57 +0300

    Added selected team profile view component

---
+++ AspNetCoreSample/Components/SelectedTeamProfile.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreSample.Models;
+using AspNetCoreSample.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSample.Components
+{
+    public class SelectedTeamProfile : ViewComponent
+    {
+        private readonly SelectedTeam _selectedTeam;
+
+        public SelectedTeamProfile(SelectedTeam selectedTeam)
+        {
+            _selectedTeam = selectedTeam;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var vm = new SelectedTeamViewModel
+            {
+                SelectedTeam = _selectedTeam,
+                SelectedTeamTotalOffer = _selectedTeam.GetSelectedTeamTotalOffer(),
+                SelectedTeamTotalRating = _selectedTeam.GetSelectedTeamTotalRating()
+            };
+
+            return View(vm);
+        }
+    }
+}
