1e8da9bbcdf83e62f56e18e516bb35c5f26c5a73
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-18 21:10:12 +0300

    Added offer controller and views with validation

---
+++ AspNetCoreSample/Controllers/OfferController.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AspNetCoreSample.Controllers
+{
+    public class OfferController : Controller
+    {
+        private readonly IOfferRepository _offerRepository;
+        private readonly SelectedTeam _selectedTeam;
+
+        public OfferController(IOfferRepository offerRepository, SelectedTeam selectedTeam)
+        {
+            _offerRepository = offerRepository;
+            _selectedTeam = selectedTeam;
+        }
+
+        public IActionResult PlaceOffer()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult PlaceOffer(Offer offer)
+        {
+            if (_selectedTeam.SelectedTeamCandidates.Count == 0)
+            {
+                ModelState.AddModelError("", "There are no candidates selected, you need to add at least one candidate to the selected team");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _offerRepository.CreateOffer(offer);
+                _selectedTeam.ClearTeam();
+                return RedirectToAction("OfferComplete");
+            }
+
+            return View(offer);
+        }
+
+        public IActionResult OfferComplete()
+        {
+            ViewBag.OfferCompleteMessage = "Your offer has been received, we shall contact you soon";
+            return View();
+        }
+    }
+}
--- AspNetCoreSample/Models/Offer.cs
+++ AspNetCoreSample/Models/Offer.cs
@@ -1,26 +1,50 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetCoreSample.Models
 {
     public class Offer
     {
+        [BindNever]
         public int ID { get; set; }
 
         public List<OfferDetail> OfferDetails { get; set; }
 
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Please enter your Company Name")]
+        [StringLength(50)]
         public string CompanyName { get; set; }
 
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            ErrorMessage = "The email address is not entered in a correct format")]
         public string Email { get; set; }
 
+        [BindNever]
+        [ScaffoldColumn(false)]
         public decimal OfferTotal { get; set; }
 
+        [BindNever]
+        [ScaffoldColumn(false)]
         public DateTime OfferPlacedDate { get; set; }
     }
 }
