06a5f9af80df943913f85f14e3e0f6f35f80714f
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-18 22:04:50 +0300

    Added authentication ui

---
+++ AspNetCoreSample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AspNetCoreSample.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetCoreSample.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetCoreSample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
