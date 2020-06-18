f2451a8fba24379fe95dc9464d9db97a60837ee8
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-18 19:10:19 +0300

    Added custom tag helper

---
+++ AspNetCoreSample/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreSample.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
