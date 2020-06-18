2bdfeb36dd61f0660a7d9b53d49de5a0d6b69ac7
Kostas Stroggylos <afempkstr@users.noreply.github.com>
2020-06-18 19:39:15 +0300

    Implemented offer repository

--- AspNetCoreSample/Models/OfferRepository.cs
+++ AspNetCoreSample/Models/OfferRepository.cs
@@ -7,9 +7,40 @@ namespace AspNetCoreSample.Models
 {
     public class OfferRepository : IOfferRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        private readonly SelectedTeam _selectedTeam;
+
+        public OfferRepository(AppDbContext appDbContext, SelectedTeam selectedTeam)
+        {
+            _appDbContext = appDbContext;
+            _selectedTeam = selectedTeam;
+        }
+
         public void CreateOffer(Offer offer)
         {
+            if (offer == null)
+            {
+                return;
+            }
 
+            var selectedTeamCandidates = _selectedTeam.SelectedTeamCandidates;
+            offer.OfferPlacedDate = DateTime.Now;
+            offer.OfferTotal = _selectedTeam.GetSelectedTeamTotalOffer();
+
+            offer.OfferDetails = new List<OfferDetail>();
+
+            foreach (var selectedTeamCandidate in selectedTeamCandidates)
+            {
+                offer.OfferDetails.Add(new OfferDetail()
+                {
+                    CandidateId = selectedTeamCandidate.Candidate.Id,
+                    OfferPrice = selectedTeamCandidate.OfferPrice
+                });
+            }
+
+            _appDbContext.Offers.Add(offer);
+            _appDbContext.SaveChanges();
         }
     }
 }
